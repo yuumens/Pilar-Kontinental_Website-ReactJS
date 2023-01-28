edce9aa3184b5c7c179c923dbcbe22c6279fd52f
YumanYummy <ahmadrizki0209@gmail.com>
2023-01-28 21:05:24 +0700

    Add More Required Navbar

--- src/components/Navbar/Navbar.jsx
+++ src/components/Navbar/Navbar.jsx
@@ -1,14 +1,31 @@
-import React from 'react';
-import pilarlogo from '../../assets/pilar0112.svg';
+import React, { useState } from 'react';
+import { RiMenu3Line, RiCloseLine } from 'react-icons/ri';
+import pilarlogo from '../../assets/pilar0111_1.svg';
+import { ig, fb, twit, yt } from './import'
 import './Navbar.css';
+
 
 const Navbar = () => {
   return (
     <div className="pilar_navbar">
       <div className="pilar_navbar-links">
         <div className="pilar_navbar-links-logo">
-          <img src={pilarlogo} alt="pilarlogo"></img>
+          <a href="#home"><img href="" src={pilarlogo} alt="pilarlogo"></img></a>
         </div>
+        <div className="pilar_navbar-links_container">
+          <p><a href="#About">ABOUT</a></p>
+          <p><a href="">SERVICES</a></p>
+          <p><a href="">CONTACT</a></p>
+        </div>
+      </div>
+      <div className="pilar_navbar-socialmedia">
+        <a href="https://www.instagram.com/pilarkontinental.official/"><img src={ig} alt="" /></a>
+        <a href="https://www.instagram.com/pilarkontinental.official/"><img src={fb} alt="" /></a>
+        <a href="https://www.instagram.com/pilarkontinental.official/"><img src={twit} alt="" /></a>
+        <a href="https://www.instagram.com/pilarkontinental.official/"><img src={yt} alt="" /></a>
+      </div>
+      <div className="pilar_navbar-menu">
+
       </div>
     </div>
   )
---
+++ src/components/Navbar/import.js
@@ -0,0 +1,8 @@
+import ig from '../../assets/SocialMedia/ig.png';
+import fb from '../../assets/SocialMedia/fb.png';
+import twit from '../../assets/SocialMedia/twit.png';
+import yt from '../../assets/SocialMedia/yt.png';
+
+export{
+    ig,fb,twit,yt,
+}
