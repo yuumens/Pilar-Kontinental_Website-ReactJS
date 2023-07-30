0d06d759f3a5ae016cdb35542cd4b26c479b1fc7
YumanYummy <ahmadrizki0209@gmail.com>
2023-07-30 23:11:57 +0700

    About Page: Making About Page Clearly and Center

--- src/components/About/About.jsx
+++ src/components/About/About.jsx
@@ -1,34 +1,31 @@
+// About.js
 import React from 'react';
-import './About.css';
+import './Abouts.css';
 
 const About = () => {
   return (
-    <div className="pilar_about section__padding" id="pilar_abouts">
-      <div className="pilar_abouts-text">
+    <div className="about-wrapper">
+      <div className="about-heading">
         <h1>About Us</h1>
-        
       </div>
-      <div className="pilar_about-filosofi">
-        <h1>Apa Itu Pilar Kontinental?</h1> 
-         
-        <h1>Mengapa Harus Di Pilar Kontinental?</h1>
-          
-      </div>
-      <div className="pilar_about-filosofis">
-          <p>
-            Pilar Kontinental adalah wadah pengembangan usaha yang bertempat di Malang, Jawa Timur. 
-            Kami sudah memulai aktivitas jasa travel & tour, konsultan hukum, creative & digital agency, dan jasa pengiriman /kargo sejak tahun 2017 dan baru di resmikan secara formal pada bulan juni 2022
-          </p>
-
-          <p>
-            Pilar Kontinental selalu memberikan wawasan kepada klien, apa yang mereka inginkan dan butuhkan untuk memenuhi kebutuhan mereka. Karena setiap orang di dalamnya ikut andil dalam menciptakan sebuah karya sehingga selaras dengan keinginan klien dan memberikan kepuasan.
-            Oleh karena itu, Pilar Kontinental selalu berkomitmen untuk memberikan pelayanan terbaik kepada setiap kliennya, baik yang tidak berwujud maupun yang berwujud.
-            Kerja sama dengan berbagai potensi yang dimiliki oleh setiap orang memberikan improvisasi inovasi yang berbeda dalam setiap karya yang dihasilkan, yang akan mampu menginspirasi banyak orang.
-          </p> 
-          
-      </div>
+        <div className="pilar_abouts-texts">
+          <div className="pilar_about-filosofi">
+            <h2>Apa Itu Pilar Kontinental?</h2>
+            <p>
+              Pilar Kontinental adalah wadah pengembangan usaha yang bertempat di Malang, Jawa Timur. Kami sudah memulai aktivitas jasa travel & tour, konsultan hukum, creative & digital agency, dan jasa pengiriman /kargo sejak tahun 2017 dan baru diresmikan secara formal pada bulan Juni 2022.
+            </p>
+          </div>
+          <div className="pilar_about-filosofis">
+            <h2>Mengapa Harus Di Pilar Kontinental?</h2>
+            <p>
+              Pilar Kontinental selalu memberikan wawasan kepada klien, apa yang mereka inginkan dan butuhkan untuk memenuhi kebutuhan mereka. Karena setiap orang di dalamnya ikut andil dalam menciptakan sebuah karya sehingga selaras dengan keinginan klien dan memberikan kepuasan.
+              Oleh karena itu, Pilar Kontinental selalu berkomitmen untuk memberikan pelayanan terbaik kepada setiap kliennya, baik yang tidak berwujud maupun yang berwujud.
+              Kerja sama dengan berbagai potensi yang dimiliki oleh setiap orang memberikan improvisasi inovasi yang berbeda dalam setiap karya yang dihasilkan, yang akan mampu menginspirasi banyak orang.
+            </p>
+          </div>
+        </div>
     </div>
-  )
-}
+  );
+};
 
-export default About
+export default About;
